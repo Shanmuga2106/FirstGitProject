cd38cbd1268a49bfb98cdfcf8b427005ddef2d06
Shanmuga2106 <vallishanmuga6@gmail.com>
2021-07-25 18:58:42 +0530

    Merge process completed

--- src/org/github/BaseClassGit.java
+++ src/org/github/BaseClassGit.java
@@ -4,6 +4,7 @@ public class BaseClassGit {
 	public static void main(String[] args) {
 		System.out.println("Created Git");
 		System.out.println("Clonning done");
+		System.out.println("Merge completed");
 	}
 
 }
